5190aea6a8296c486891e010e3bd3e7270969c8f
Omar Hussein <omarhatem101@gmail.com>
2019-09-26 13:50:21 +0200

    Font added + starting on sign in

--- lib/main.dart
+++ lib/main.dart
@@ -26,48 +26,53 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.amber,
         accentColor: Colors.amber,
-        canvasColor: Color.fromRGBO(255, 254, 229, 1),
-        // fontFamily: 'Raleway',
+        canvasColor: Color(0xffFECC33),
+        fontFamily: 'Poppins',
         textTheme: ThemeData.light().textTheme.copyWith(
             body1: TextStyle(
               color: Color.fromRGBO(20, 51, 51, 1),
             ),
             body2: TextStyle(
               color: Color.fromRGBO(20, 51, 51, 1),
-            )
-            // title: TextStyle(
-            //   fontSize: 20,
-            //   fontFamily: 'RobotoCondensed',
-            //   fontWeight: FontWeight.bold,
-            // )
             ),
+            title: TextStyle(
+              fontFamily: 'Poppins',
+              fontSize: 70,
+              fontWeight: FontWeight.w900,
+            ),
+            subtitle: TextStyle(
+              fontFamily: 'Poppins',
+              fontSize: 20,
+              fontWeight: FontWeight.w900,
+            )),
       ),
       // home: CategoriesScreen(),
       initialRoute: '/', // default is '/'
       routes: {
-        '/': (ctx) => SignInScreen(),
+        '/': (ctx) => SignIn(),
         AddTourScreen.routeName: (ctx) => AddTourScreen(),
         ChatScreen.routeName: (ctx) => ChatScreen(),
         InboxScreen.routeName: (ctx) => InboxScreen(),
         SearchToursScreen.routeName: (ctx) => SearchToursScreen(),
         SignUpTourGuideScreen.routeName: (ctx) => SignUpTourGuideScreen(),
         SignUpTouristScreen.routeName: (ctx) => SignUpTouristScreen(),
-        TourTourGuideDetailedScreen.routeName: (ctx) => TourTourGuideDetailedScreen(),
-        TourTouristDetailedScreen.routeName: (ctx) => TourTouristDetailedScreen(),
+        TourTourGuideDetailedScreen.routeName: (ctx) =>
+            TourTourGuideDetailedScreen(),
+        TourTouristDetailedScreen.routeName: (ctx) =>
+            TourTouristDetailedScreen(),
         ToursGuideHomePageScreen.routeName: (ctx) => ToursGuideHomePageScreen(),
         TourGuideProfileScreen.routeName: (ctx) => TourGuideProfileScreen(),
         ToursitHomePageScreen.routeName: (ctx) => ToursitHomePageScreen(),
         ToursitProfileScreen.routeName: (ctx) => ToursitProfileScreen(),
         ToursTouristScreen.routeName: (ctx) => ToursTouristScreen(),
         ToursTourGuideScreen.routeName: (ctx) => ToursTourGuideScreen(),
-
       },
       onGenerateRoute: (settings) {
         print(settings.arguments);
       },
       onUnknownRoute: (settings) {
         return MaterialPageRoute(
-          builder: (ctx) => SignInScreen(),
+          builder: (ctx) => SignIn(),
         );
       },
     );
